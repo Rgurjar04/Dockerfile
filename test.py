7cdf543bc72ae7356aa144aa2600123cb9f842b6
Rgurjar04 <76676739+Rgurjar04@users.noreply.github.com>
2021-01-23 18:16:02 +0530

    python_code

---
+++ test.py
@@ -0,0 +1,7 @@
+print("\t\t\t\tWelcome")
+print("\t\t\t-------------------------")
+print("\t\t\tThis is Rupali Gurjar")
+c = input("Please Enter 'q' to quit :")
+if (c=='q'):
+        print("Successfully tested !!")
+        exit()
